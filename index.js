00bd3ef0399dd11cfaba720a08843a2437a1949c
al3ss4ndrolb <173555546+al3ss4ndrolb@users.noreply.github.com>
2024-12-29 00:19:00 +0100

    feat: update operate function

--- index.js
+++ index.js
@@ -4,11 +4,30 @@ const subtract = (a, b) => a - b;
 
 const multiply = (a, b) => a * b;
 
-const divide = (a, b) => a / b;
+const divide = (a, b) => {
+  if (b === 0) {
+    return "Error: Cannot divide by zero";
+  }
 
-let firstNumber = Number;
-let secondNumber = Number;
+  return a / b;
+};
+
+let firstNumber = 0;
+let secondNumber = 0;
 let operator;
 
-const operate = (operator, firstNumber, secondNumber) =>
-  add || subtract || multiply || divide;
+const operate = (operator, firstNumber, secondNumber) => {
+  switch (operator) {
+    case "+":
+      return add(firstNumber, secondNumber);
+    case "-":
+      return subtract(firstNumber, secondNumber);
+    case "*":
+      return multiply(firstNumber, secondNumber);
+    case "/":
+      return divide(firstNumber, secondNumber);
+
+    default:
+      return alert("invalid operator");
+  }
+};
